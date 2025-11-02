0b53f2d2c17fc00a660e0e7363755731aba8cb29
reppybr <reppybr@gmail.com>
2025-11-02 20:25:42 -0300

    Update run.py

--- run.py
+++ run.py
@@ -4,4 +4,12 @@ import os
 app = create_app(os.environ.get('FLASK_ENV', 'development'))
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=5000, debug=True)
+    # Render (e outros) define a porta pela variável de ambiente PORT
+    port = int(os.environ.get('PORT', 5000))
+    
+    # Debug deve ser Falso em produção
+    # O Render define FLASK_ENV=production por padrão
+    is_debug = os.environ.get('FLASK_ENV') != 'production'
+
+    # 👇 A MUDANÇA PRINCIPAL ESTÁ AQUI
+    app.run(host="0.0.0.0", port=port, debug=is_debug)
